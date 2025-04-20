13b0fc2562f380828a632b09fb10a017d9127946
Shokha-cs <xoliyorovshahriyor@gmail.com>
2025-04-20 11:18:35 +0900

    javascript in calculator

--- styles/calculator.js
+++ styles/calculator.js
@@ -1,24 +1,21 @@
+// document.getElementById("info-form").addEventListener("submit", function(e) {
+//   e.preventDefault(); // stop the page from reloading
 
-document.getElementById("info-form").addEventListener("submit", function(e) {
-  e.preventDefault(); // stop the page from reloading
+//   // Get values from inputs
+//   const gender = document.getElementById("Gender").value;
+//   const age = document.getElementById("age").value;
+//   const weight = document.getElementById("weight").value;
+//   const height = document.getElementById("length").value;
+//   const goal = document.getElementById("goal").value;
+//   const workouts = document.getElementById("workouts").value;
 
-  // Get values from inputs
-  const gender = document.getElementById("Gender").value;
-  const age = document.getElementById("age").value;
-  const weight = document.getElementById("weight").value;
-  const height = document.getElementById("length").value;
-  const goal = document.getElementById("goal").value;
-  const workouts = document.getElementById("workouts").value;
-
-  // Show the values below the form
-  document.getElementById("result").innerHTML = `
-    🧍 <strong>Gender:</strong> ${gender} <br>
-    🎂 <strong>Age:</strong> ${age} <br>
-    ⚖️ <strong>Weight:</strong> ${weight} kg <br>
-    📏 <strong>Height:</strong> ${height} cm <br>
-    🎯 <strong>Goal:</strong> ${goal === "lose" ? "Lose weight" : "Gain muscle"} <br>
-    💪 <strong>Workouts per week:</strong> ${workouts}
-  `;
-});
-
-
+//   // Show the values below the form
+//   document.getElementById("result").innerHTML = `
+//      <strong>Gender:</strong> ${gender} <br>
+//      <strong>Age:</strong> ${age} <br>
+//      <strong>Weight:</strong> ${weight} kg <br>
+//      <strong>Height:</strong> ${height} cm <br>
+//      <strong>Goal:</strong> ${goal === "lose" ? "Lose weight" : "Gain muscle"} <br>
+//      <strong>Workouts per week:</strong> ${workouts}
+//   `;
+// });
