d57c5a2fa252ed301e44ab93855681974fedaf3d
rens-collab <rensvanputten@ziggo.nl>
2025-05-31 22:49:50 +0900

    Added react and backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -8,4 +8,6 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port http://localhost:3000/ || ${PORT}`)
+);
