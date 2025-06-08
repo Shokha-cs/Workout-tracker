14d96e5de2bdc2941ffbb820f958f18d8f4698ff
Shokha-cs <xoliyorovshahriyor@gmail.com>
2025-06-08 19:34:35 +0900

    git

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
-
 
 app.use((req, res, next) => {
   console.log("Request received:", req.method, req.url);
   next();
 });
 
-const quoteRoutes = require("./routes/quotes"); 
-app.use("/api/quotes", quoteRoutes);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT = 5000;
+app.use("/api/quotes", require("./routes/quotes"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
