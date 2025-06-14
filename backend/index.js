19e30dede8c1ee3b15a0d987dd2f2bb5ed961c7e
rens-collab <rensvanputten@ziggo.nl>
2025-06-14 17:31:56 +0900

    Fixed all

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("node:path")
-
+const path = require("node:path");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://https://momentum-pink-theta.vercel.app/" }));
 
 app.use((req, res, next) => {
   console.log("Request received:", req.method, req.url);
@@ -15,13 +14,7 @@ app.use((req, res, next) => {
 const quoteRoutes = require("./routes/quotes");
 app.use("/api/quotes", quoteRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("/*path", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-});
-
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
